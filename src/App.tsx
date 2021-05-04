cad66983636500126ae0c141e4e7812d4878b039
Kevin Prakasa <kevinprakasa03@gmail.com>
2021-05-04 21:18:00 +0700

    Add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "@progress/kendo-theme-default/dist/all.css";
 import { Calendar } from "@progress/kendo-react-dateinputs";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Calendar />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+              <li>
+                <Link to="/register">register</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <div>login</div>
+              <Calendar />
+            </Route>
+            <Route path="/register">
+              <div>register</div>
+            </Route>
+            <Route path="/">
+              <div>root</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
